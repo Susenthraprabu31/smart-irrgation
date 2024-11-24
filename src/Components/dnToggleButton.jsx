fa383e3476f9e816d7447976b82da0d9c4d65aa1
Susenthraprabu31 <susenthraprabu2002@github.com>
2024-11-24 22:48:18 +0530

    smart-irrgation

--- src/Components/dnToggleButton.jsx
+++ src/Components/dnToggleButton.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import "./dnToggle.css"
 const dnToggleButton = () => {
     // Manage theme state
     const [isDaylight, setIsDaylight] = useState(() => {
